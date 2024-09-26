f973114e76709f8ae7fd23d0a830182d603d7317
Vachek-hooves <vachek0602@gmail.com>
2024-09-26 17:46:57 +0300

    update emotional waterfalls

--- data/EmotionalFallse.js
+++ data/EmotionalFallse.js
@@ -1,1 +1,98 @@
-export const EmotionalFalls = [];
+export const EmotionalFalls = [
+  {
+    name: "Victoria Falls",
+    emotion: "Romance",
+    emotionEmoji: "💖",
+    description: "Victoria Falls, located on the border between Zambia and Zimbabwe, is one of the largest waterfalls in the world by volume of falling water. This waterfall is famous for its beauty and romantic atmosphere, as its waters often form a rainbow during a sunny day. Victoria is surrounded by abundant tropical vegetation, which adds additional charm and color. The sounds of water falling loudly together with beautiful views create an ideal place for romantic walks and relaxing together.",
+    coordinates: {
+      latitude: -17.9243,
+      longitude: 25.8572
+    },
+    countries: ["Zambia", "Zimbabwe"],
+    image: "https://africanbushcamps.com/wp-content/uploads/2022/02/victoria-falls-5903496_1280-1024x683.webp"
+  },
+  {
+    name: "Yosemite Falls",
+    emotion: "Majesty",
+    emotionEmoji: "😮",
+    description: "Yosemite Falls, located in Yosemite National Park in California, is one of the highest waterfalls in North America. With a height of over 740 meters, it impresses with its grandeur and dynamic appearance. During the spring flood or after heavy rains, its waters form a huge cascade that creates a powerful sound and breathtaking scenery. Yosemite is a symbol of natural power and beauty that inspires visitors and leaves an unforgettable impression.",
+    coordinates: {
+      latitude: 37.7565,
+      longitude: -119.5962
+    },
+    countries: ["United States"],
+    image: "https://www.nps.gov/common/uploads/cropped_image/primary/944B8190-DC70-F04B-19C33DF399EFF953.jpg?width=1600&quality=90&mode=crop"
+  },
+  {
+    name: "Iguazu Falls",
+    emotion: "Drama",
+    emotionEmoji: "🌪️",
+    description: "Iguazu Falls, located on the border of Argentina and Brazil, is one of the largest and most impressive waterfalls in the world. It consists of more than 270 separate cascades forming a huge wall of water. The sounds of water falling into deep gorges create a dramatic atmosphere, and the sheer volume of water is stunning. Iguazu impresses with its diversity and scale, offering visitors incredible impressions and emotions.",
+    coordinates: {
+      latitude: -25.6953,
+      longitude: -54.4367
+    },
+    countries: ["Argentina", "Brazil"],
+    image: "https://iguazufalls.com/wp-content/uploads/bg-new-demo-iguazu.jpg"
+  },
+  {
+    name: "Selfoss",
+    emotion: "Tranquility",
+    emotionEmoji: "🌊",
+    description: "Selfoss waterfall, located in the north of Iceland, is known for its calm and peaceful atmosphere. It differs from other waterfalls by its soft and smooth flow, which flows over black lava rocks. Its quiet sound and minimalist appearance create an atmosphere of peace and harmony, ideal for those looking for a place to meditate or relax.",
+    coordinates: {
+      latitude: 65.6409,
+      longitude: -16.1834
+    },
+    countries: ["Iceland"],
+    image: "https://e498rczdjg6.exactdn.com/wp-content/uploads/2020/09/Selfoss-waterfall-2.jpg?strip=all&lossy=1&ssl=1"
+  },
+  {
+    name: "Rhine Falls",
+    emotion: "Invigoration",
+    emotionEmoji: "😊",
+    description: "The Rhine Falls, located in Germany, is one of the largest and most famous waterfalls in Europe. Its water falls from a height of about 23 meters, creating a lively and energetic flow. Rhine Falls is famous for its freckles and dynamic appearance, surrounded by beautiful natural landscapes. Its visit charges positive energy and brings a feeling of vigor and recovery.",
+    coordinates: {
+      latitude: 47.6778,
+      longitude: 8.6150
+    },
+    countries: ["Switzerland", "Germany"],
+    image: "https://switzerland-tour.com/storage/media/Rhine%20Falls/Rhine-Falls-in-Switzerland.jpg"
+  },
+  {
+    name: "Tugela Falls",
+    emotion: "Inspiration",
+    emotionEmoji: "🌟",
+    description: "Tugela Falls, located in South Africa, is the second highest waterfall in the world. Its waters fall from a height of more than 900 meters, creating impressive views and majestic cascades. Tugela impresses with its beauty and scale, inspiring visitors to new ideas and creative thinking. Its majestic appearance promotes creativity and dreaminess.",
+    coordinates: {
+      latitude: -28.7489,
+      longitude: 28.8942
+    },
+    countries: ["South Africa"],
+    image: "https://farsouthtours.com/wp-content/uploads/2022/08/Tugela-falls_fcbk-scaled.jpg"
+  },
+  {
+    name: "Angel Falls",
+    emotion: "Mystique",
+    emotionEmoji: "🌫️",
+    description: "Angel Falls, located in Venezuela, is the highest waterfall in the world. The water falls from a height of more than 3,000 meters, creating a breathtaking and mystical appearance. The waterfall is located in a remote jungle where its waters are surrounded by mist and clouds. This place creates a sense of charm and mystery, making it ideal for exploration and exciting adventures.",
+    coordinates: {
+      latitude: 5.9701,
+      longitude: -62.5362
+    },
+    countries: ["Venezuela"],
+    image: "https://www.journeylatinamerica.com/app/uploads/destinations/venezuela/angel-falls-and-the-gran-sabana/ven_angelfalls_shutterstock_141881491-scaled-1200x800-c-center.jpg"
+  },
+  {
+    name: "Grand Canyon Waterfall",
+    emotion: "Adventure",
+    emotionEmoji: "🏞️",
+    description: "The waterfall in the Grand Canyon, located in the USA, is part of one of the largest and most famous natural wonders of the world. Its waters fall into deep gorges, creating fascinating landscapes and unpredictable emotions.",
+    coordinates: {
+      latitude: 36.0544,
+      longitude: -112.1401
+    },
+    countries: ["United States"],
+    image: "https://buckwildhummertours.com/wp-content/uploads/sites/6808/2021/12/Get-to-Know-the-Grand-Canyon-Secrets-and-the-Hidden-Waterfall.jpg?w=700&h=700&zoom=2"
+  }
+];
